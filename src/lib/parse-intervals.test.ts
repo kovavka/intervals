7c6a10eaf78d0be9d558f213dd365deddab1ee1f
Gin Kapger <gikapger@gmail.com>
2025-09-01 17:44:04 +0200

    Parse intervals

--- src/lib/parse-intervals.test.ts
+++ src/lib/parse-intervals.test.ts
@@ -18,7 +18,7 @@ describe('Parse intervals input', () => {
 
   describe('Should return empty array', () => {
     it('When input is empty array JSON', () => {
-      expect(parseIntervalsInput('null')).toEqual([])
+      expect(parseIntervalsInput('[]')).toEqual([])
     })
   })
 
@@ -37,50 +37,63 @@ describe('Parse intervals input', () => {
 
     describe('Invalid interval', () => {
       it('When interval is not an array', () => {
-        expect(() => parseIntervalsInput('[[1, 2], 1]')).toThrow('Invalid')
+        expect(() => parseIntervalsInput('[[1, 2], 1]')).toThrow('1 is not a valid interval')
       })
       it('When interval is empty array', () => {
-        expect(() => parseIntervalsInput('[[1, 2], []]')).toThrow('Invalid')
+        expect(() => parseIntervalsInput('[[1, 2], []]')).toThrow('[] is not a valid interval')
       })
       it("When at least one of the arrays' length is not 2", () => {
-        expect(() => parseIntervalsInput('[[2, 4, 3]')).toThrow('Invalid')
+        expect(() => parseIntervalsInput('[[2, 4, 3]]')).toThrow(
+          '[2,4,3] is not a valid interval'
+        )
       })
       it('When interval contains string value', () => {
-        expect(() => parseIntervalsInput('[[1, "2"]]')).toThrow('Invalid')
+        expect(() => parseIntervalsInput('[[1, "2"]]')).toThrow(
+          '[1,"2"] is not a valid interval'
+        )
       })
       it('When interval contains boolean value', () => {
-        expect(() => parseIntervalsInput('[[1, true]]')).toThrow('Invalid')
+        expect(() => parseIntervalsInput('[[1, true]]')).toThrow(
+          '[1,true] is not a valid interval'
+        )
       })
       it('When interval contains null', () => {
-        expect(() => parseIntervalsInput('[[1, null]]')).toThrow('Invalid')
+        expect(() => parseIntervalsInput('[[1, null]]')).toThrow(
+          '[1,null] is not a valid interval'
+        )
       })
       it('When interval contains an object', () => {
-        expect(() => parseIntervalsInput('[[1, {}]]')).toThrow('Invalid')
+        expect(() => parseIntervalsInput('[[1, {}]]')).toThrow('[1,{}] is not a valid interval')
+      })
+      it('When interval value is bigger than safe integer', () => {
+        expect(() =>
+          parseIntervalsInput('[[1, 923939920390329023902390239032902390]]')
+        ).toThrow('[1,9.23939920390329e+35] is not a valid interval')
       })
       it("When interval's end is less than start", () => {
-        expect(() => parseIntervalsInput('[[5, 2]]')).toThrow('Invalid')
+        expect(() => parseIntervalsInput('[[5, 2]]')).toThrow('[5,2] is not a valid interval')
       })
     })
   })
 
   describe('Should return an array of provided intervals', () => {
     it('When JSON array contains one correct interval', () => {
-      expect(parseIntervalsInput('[1, 2]')).toEqual([[1, 2]])
+      expect(parseIntervalsInput('[[1, 2]]')).toEqual([[1, 2]])
     })
     it('When JSON array contains several correct intervals', () => {
-      expect(parseIntervalsInput('[1, 2], [6, 10]')).toEqual([
+      expect(parseIntervalsInput('[[1, 2], [6, 10]]')).toEqual([
         [1, 2],
         [6, 10],
       ])
     })
     it('When intervals contain negative numbers or zero', () => {
-      expect(parseIntervalsInput('[-10, -1], [0, 1]')).toEqual([
+      expect(parseIntervalsInput('[[-10, -1], [0, 1]]')).toEqual([
         [-10, -1],
         [0, 1],
       ])
     })
     it('When one of the intervals is of length one', () => {
-      expect(parseIntervalsInput('[1, 1]')).toEqual([[1, 1]])
+      expect(parseIntervalsInput('[[1, 1]]')).toEqual([[1, 1]])
     })
   })
 })
